cff72d6a9306944186c0beadb8a662e9bcdc1d2d
Tim <timothy.w.holman@gmail.com>
2017-05-03 12:27:59 +0200

    base demo, mildly working

---
+++ demo/js/index.js
@@ -0,0 +1,25 @@
+(function() {
+
+  var header    = document.querySelector('h1');
+  var selectBox = document.querySelector('select');
+  var runButton = document.querySelector('button');
+
+  // On SelectBox Change
+  selectBox.addEventListener('change', function() {
+    triggerAnimation();
+  })
+
+  // On "run animation" button clicked
+  runButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    triggerAnimation();
+  })
+
+  function triggerAnimation() {
+
+    var animation = selectBox.value;
+    header.className = "animated " + animation;
+
+  }
+
+})();
